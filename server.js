7c04c2a1e39630cb4167ab7204680e3168228f5a
Atanda Nafiu <atandanafiu21@gmail.com>
2022-11-02 01:14:38 +0100

    fix

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require("express");
-var app = express();
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+const port = process.env.PORT || 5000
+
+const app = express();
+
+
 
 
 app.get('/', (req, res) => {
     res.send(
-    { slackUsername: "atanda0x", 
-    backend: true,
-    age: 20, 
-    bio: "I'm tall, dark and god look" })
+    { "slackUsername": "atanda0x", 
+    "backend": true,
+    "age": 20, 
+    "bio": "I'm tall, dark and good looking" })
 });
 
-app.listen(3000)
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+})
